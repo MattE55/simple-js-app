bf3b7995f9fe5374934e62a2a2ddf610446405f3
MattE55 <89932886+MattE55@users.noreply.github.com>
2021-09-23 14:36:41 -0700

    Starting the Plkedex
    Attempting to make the pokemonlist variable work

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,2 @@
-alert('Hello World');
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonlist = [{name:"bulbasaur",type:["grass","poison"], height:2},{name:"pikachu",type:"electric",height:1,},{name:"machamp",type:"fighting",height:5}]
+console.log(pokemonlist)
