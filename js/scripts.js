74450b0024a5ad5e14431415d3a29b95cd432d59
MattE55 <89932886+MattE55@users.noreply.github.com>
2021-09-23 14:40:22 -0700

    Updating Pokedex
    changed the variable from pokemonlist to pokemonList like the assignment described. I may change it back to avoid capital letters for the variable eventually.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,2 @@
-let pokemonlist = [{name:"bulbasaur",type:["grass","poison"], height:2},{name:"pikachu",type:"electric",height:1,},{name:"machamp",type:"fighting",height:5}]
-console.log(pokemonlist)
+let pokemonList = [{name:"bulbasaur",type:["grass","poison"], height:2},{name:"pikachu",type:"electric",height:1,},{name:"machamp",type:"fighting",height:5}]
+console.log(pokemonList)
