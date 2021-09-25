b545a206a653d49baecdf02421d2a2217d32e95a
MattE55 <89932886+MattE55@users.noreply.github.com>
2021-09-25 15:49:16 -0700

    Starated Creating The Pokedex
    Small issue, browser console is recognizing the size of the pokemon but I am not sure how to JS documentwrite it onto the page. It is only showing up as object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,35 @@
-let pokemonList = [{name:"bulbasaur",type:["grass","poison"], height:2},{name:"pikachu",type:"electric",height:1,},{name:"machamp",type:"fighting",height:5}]
+let pokemonList = [
+  {name:"bulbasaur",type:["grass","poison"], height:2},
+  {name:"pikachu",type:"electric",height:0.9},
+  {name:"machamp",type:"fighting",height:6}
+  ]
 console.log(pokemonList)
+
+for (let i=0; i < pokemonList.length; i++){
+  if (pokemonList[i].height <5 && pokemonList[i].height >1){
+    console.log(pokemonList[i].name + " is an average Pokemon");
+  }else if (pokemonList[i].height >5){
+    console.log(pokemonList[i].name + " is a large Pokemon");
+  }else {
+    console.log(pokemonList[i].name + " is a small Pokemon");
+  }
+}
+document.write(pokemonList)
+
+let repository = [
+  {
+    name: "bulbasaur",
+    height: 2,
+    type: ["grass", "poison"]
+  },
+  {
+    name: "pikachu",
+    height: 0.9,
+    type: ["electric"]
+  },
+  {
+    name: "machamp",
+    height: 6,
+    type: ["fighting"]
+  }
+]
