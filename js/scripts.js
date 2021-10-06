783ccfcdc189683307934e0d1a4bb965b4cb3c3b
MattE55 <89932886+MattE55@users.noreply.github.com>
2021-10-06 14:44:29 -0700

    Big pokedex update
    added fetch and promise polyfill javascript files.
    
    Added the pokemon api to the scripts js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,21 @@
-
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: 'bulbasaur',
-      type: ['grass','poison'],
-      height: 2
-    },
-    {
-      name: 'pikachu',
-      type: 'electric',
-      height: 0.9
-    },
-    {
-      name: 'machamp',
-      type: ['fire','flying'],
-      height: 6
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon,
+      "detailsUrl" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct")
     }
-  ];
+  }
 
   function getAll() {
     return pokemonList;
-  }
-
-  function add(pokemon) {
-    pokemonList.push(pokemon);
   }
 
   function addListItem(pokemon){
@@ -32,34 +24,62 @@ let pokemonRepository = (function () {
     let button = document.createElement("button");
     button.innerText = pokemon.name;
     button.classList.add("button-class");
-    //button.addEventListener('click', function showDetails
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
-
     button.addEventListener("click", function () {
       showDetails(pokemon);
     });
   }
 
-  function showDetails(pokemon){
-    console.log(pokemon.name)
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  function showDetails(item){
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
   }
 
   return {
-    getAll: getAll,
     add: add,
+    getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
     showDetails: showDetails
   };
 })();
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({
-  name: 'charmander',
-  type: 'fire',
-  height: 2
-});
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
